e585505f9130860f5e021b5452b83e6a986d3a76
Dennis Abrikossov <Darkdennis1000@gmail.com>
2026-03-03 17:59:19 +0100

    fixed a bug by moving readt

--- frontend/src/components/atoms/StatusBadge.tsx
+++ frontend/src/components/atoms/StatusBadge.tsx
@@ -1,25 +1,25 @@
-import Spinner from "./Spinner";
+import Spinner from './Spinner';
 
 interface StatusBadgeProps {
-    isProcessed: boolean;
+  isProcessed: boolean;
 }
 
-const StatusBadge = ({isProcessed}: StatusBadgeProps) =>{
-    if (isProcessed) {
-        return (   <div className="flex items-center gap-2 text-emerald-400 text-xs font-medium bg-emerald-400/10 px-2 py-1 rounded-full w-fit border-emerald-400/20 ">
-            <div className="w-3 h-3 rounded-full bg-emerald-400">
-                Ready
-            </div>
-        </div>
-        );
-    }
-
+const StatusBadge = ({ isProcessed }: StatusBadgeProps) => {
+  if (isProcessed) {
     return (
-        <div className="flex items-center gap-2 text-amber-400 text-xs font-medium bg-amber-400/10 px-2 py-1 rounded-full w-fit border border-amber-400/20 animate-pulse">
-            <Spinner className="w-3 h-3 text-amber-400"/>
-            Processing
-        </div>
+      <div className="flex items-center gap-2 text-emerald-400 text-xs font-medium bg-emerald-400/10 px-2 py-1 rounded-full w-fit border border-emerald-400/20 ">
+        <div className="w-1.5 h-3 rounded-full bg-emerald-400"></div>
+        Ready
+      </div>
     );
+  }
+
+  return (
+    <div className="flex items-center gap-2 text-amber-400 text-xs font-medium bg-amber-400/10 px-2 py-1 rounded-full w-fit border border-amber-400/20 animate-pulse">
+      <Spinner className="w-3 h-3 text-amber-400" />
+      Processing
+    </div>
+  );
 };
 
-export default StatusBadge
+export default StatusBadge;
--- frontend/src/screens/DetatilsScreen.tsx
+++
@@ -1,6 +0,0 @@
-import type React from "react";
-
-const DetailsScreen: React.FC = () => {
-
-}
-export default DetailsScreen;
